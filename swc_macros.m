bad2e9f07fe113224ce7c994d412dfa0ed59dd0b
Supe <nctzv8@splm.siemens.com>
2019-10-24 16:01:42 -0400

    Adding files to master

---
+++ swc_macros.m
@@ -0,0 +1,23 @@
+SWC_VER_IMP = Simulink.Parameter;
+SWC_VER_IMP.DataType = 'uint8';
+SWC_VER_IMP.Min = 0;
+SWC_VER_IMP.Max = 255;
+SWC_VER_IMP.Description = 'Implementation version';
+SWC_VER_IMP.DocUnits = 'Unitless';
+SWC_VER_IMP.Value = 0;
+
+SWC_VER_MINOR = Simulink.Parameter;
+SWC_VER_MINOR.DataType = 'uint8';
+SWC_VER_MINOR.Min = 0;
+SWC_VER_MINOR.Max = 255;
+SWC_VER_MINOR.Description = 'SWC Minor Version number';
+SWC_VER_MINOR.DocUnits = 'Unitless';
+SWC_VER_MINOR.Value = 0;
+
+SWC_VER_MAJOR = Simulink.Parameter;
+SWC_VER_MAJOR.DataType = 'uint8';
+SWC_VER_MAJOR.Min = 0;
+SWC_VER_MAJOR.Max = 255;
+SWC_VER_MAJOR.Description = 'SWC Major Version number for Stage 1 ';
+SWC_VER_MAJOR.DocUnits = 'Unitless';
+SWC_VER_MAJOR.Value = 2;
